57e2906bc1a339d2cf8a26bd23155958a5fdde44
asifalmani <asif.shahzad@coeus-solutions.de>
2016-07-25 16:43:26 +0500

    Adding all files in the GIT repo

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require("gulp-rename");
+var minifyCss = require('gulp-minify-css');
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles', function(){
+	gulp.src('./scss/main.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./css'))
+	.pipe(minifyCss())
+	.pipe(rename({extname: '.min.css'}))
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('serve', function (){
+	browserSync.init({
+		server: {
+			baseDir:'./'
+		}
+	})
+
+	gulp.watch('./scss/*',['styles']);
+	gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'serve']);
